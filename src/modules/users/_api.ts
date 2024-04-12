e2128eece9bc79092b97418db06f6ba91c4d4e24
MirzayevOrzu <mirzayevorzu127@gmail.com>
2024-04-12 18:20:03 +0500

    fix(users): make edit profile work

--- src/modules/users/_api.ts
+++ src/modules/users/_api.ts
@@ -19,8 +19,8 @@ usersRouter.post("/users", hasRole([UserRole.ADMIN]), postUser);
 usersRouter.get("/users", hasRole([UserRole.ADMIN]), getUsers);
 usersRouter.get("/users/profile", getProfile);
 usersRouter.get("/users/:id", hasRole([UserRole.ADMIN]), getUser);
-usersRouter.patch("/users/:id", hasRole([UserRole.ADMIN]), patchUser);
 usersRouter.patch("/users/profile", patchProfile);
+usersRouter.patch("/users/:id", hasRole([UserRole.ADMIN]), patchUser);
 usersRouter.delete("/users/:id", hasRole([UserRole.ADMIN]), deleteUser);
 
 export default usersRouter;
